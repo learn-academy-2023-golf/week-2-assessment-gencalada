6ff72b161ce06b42e55a4397daff7decd93c80ca
Greg <gvasquezencalada@gmail.com>
2023-10-16 08:03:22 -0700

    week 2 assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,41 +11,110 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
+// describe('multipliedByThree',() => {
+//     it('takes in an array of numbers and returns an array with all the numbers multiplied by 3',() => {
+//         const numbersArray1 = [6, 7, 8, 9, 10]
+//         // Expected output: [18, 21, 24, 27, 30]
+//         const numbersArray2 = [24, 27, 30, 33, 36]
+//         // Expected output: [72, 81, 90, 99, 108]
+//         expect(multipliedByThree(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+//         expect(multipliedByThree(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+//     })
+// })
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
 
-// b) Create the function that makes the test pass.
+// // const numbersArray1 = [6, 7, 8, 9, 10]
+// // // Expected output: [18, 21, 24, 27, 30]
+// // const numbersArray2 = [24, 27, 30, 33, 36]
+// // // Expected output: [72, 81, 90, 99, 108]
 
-// Pseudo code:
+// // b) Create the function that makes the test pass.
+
+// // Pseudo code:
+// //input:
+// //output:
+// //iterate through the array using .map
+// //each interaction multiplies the current value by three
+// //the product is returned in the new array made
+
+// const multipliedByThree = (arr) => {
+//     return arr.map(num => {
+//         return num * 3
+//     })
+// }
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
+// describe('divisibleByThree',() => {
+//     it('takes a object as an argument and decides if the number inside it is evenly divisible by three or not',() => {
+//         const object1 = { number: 15 }
+//         // Expected output: "15 is divisible by three"
+//         const object2 = { number: 0 }
+//         // Expected output: "0 is divisible by three"
+//         const object3 = { number: -7 }
+//         // Expected output: "-7 is not divisible by three"
+//         expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
+//         expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
+//         expect(divisibleByThree(object3)).toEqual("-7 is not divisible by three")
+//     })
+// })
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+// // const object1 = { number: 15 }
+// // // Expected output: "15 is divisible by three"
+// // const object2 = { number: 0 }
+// // // Expected output: "0 is divisible by three"
+// // const object3 = { number: -7 }
+// // // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// // Pseudo code:
+// //Input: An object with numbers
+// //Output: An object that decides if the numbers inside are divisible by three
+// //using % iterate through each object's number
+// //using string interpelation, each product returns a string describing the outcome
+
+// const divisibleByThree = (string) => {
+//     let num1 = object1
+//     let num2 = object2
+//     let num3 = object3
+//     if(num1 % 3 === 0) {
+//         retrun `${num1} is divisible by three`
+//     }
+//     if(num2 % 3 === 0) {
+//         return `${num2} is divisible by three`
+//     }
+//     if(num3 %  3 === 0) {
+//         return `${num3} is divisible by three`
+//     }
+// }
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
-
 // a) Create a test with expect statements for each of the variables provided.
+describe('nounsCapetalized', () => {
+    it('takes in an array of words and returns an array with all the words capitalized')
+    const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+    // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+    const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+    // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+    expect(nounsCapetalized(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+    expect(nounsCapetalized(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+})
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+// // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//input: an array of words
+//output: an array with all words capetalized
+//using the .toUpperCase we will capetalize all input to receive expected output
+
+const nounsCapetalized = (arr) => {
+    return (arr.charAt(0)).toUpperCase
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -9,28 +9,28 @@
 // --------------------1) What will this log?
 
 const cohort = "Golf 2023"
-// console.log(cohort.split(" "))
+console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The output will be a new string where each letter its going to to separate. example: G o l f 2 0 2 3
+// b) Verify and explain: This actually gave us an array with the two elements "Golf" and "2023". split is a string method that will return an array. I was condusing this with a different method.
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: `Hello LEARN Student` will be returned
+// b) Verify and explain: I was wrong. the output was actually undefuned. this is probbly due to 'name' not being given a value.
 
 // --------------------3) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the output will be a new array with the numbers [11, 13, 15] in it.
+// b) Verify and explain: I was correct. This is because whats being filtered or nulled out of the array is all numbers that are divisible by 2. you can see this in number % 2 !==0.
 
 // --------------------4) What will this log?
 
@@ -40,10 +40,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 'Javascript' will be returned
+// b) Verify and explain: I was correct. This is because, in the console.log, we are searching for what is taking uo the zero inde inside the language array.
 
 // --------------------5) What will this log?
 
@@ -55,7 +55,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined will the displayed.
+// b) Verify and explain: i was incorrect. Gearge was dispayed. I simply had my doubts. Since this.student is the only iten that can take a new value.
